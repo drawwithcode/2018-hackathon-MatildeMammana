3e91164886dc1d534482dc0abff116f0f498c7e5
MatildeMammana <matilde.mammana@gmail.com>
2018-11-28 12:45:36 +0100

    Completed

--- sketch.js
+++ sketch.js
@@ -1,11 +1,51 @@
-function preload(){
-  // put preload code here
+var mySong;
+var img = [];
+
+function preload() {
+  mySong = loadSound("./assets/How I Met Your Mother Opening Credits True HD.mp3")
+  img[0] = loadImage("./assets/volume0.png");
+  img[1] = loadImage("./assets/volume1.png");
+  img[2] = loadImage("./assets/volume2.png");
+  img[3] = loadImage("./assets/volume.png");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
+  imageMode(CENTER);
+  mySong.loop();
+
 }
 
 function draw() {
-  // put drawing code here
+
+  var myText = 'Muoviti lungo asse x per aumentare e abbassare il volume'
+  textSize(25);
+  textAlign(CENTER);
+  textFont('Bitter');
+  fill('black');
+  text(myText, width / 2, 100);
+
+
+  if (mouseX < width / 4) {
+    image(img[0], width / 2, height / 2, img[0].width / 2, img[0].height / 2);
+  } else {
+    if (mouseX > width / 4 && mouseX < width / 2) {
+      image(img[1], width / 2, height / 2, img[0].width / 2, img[0].height / 2);
+    } else {
+      if (mouseX > width / 2 && mouseX < width / 2 + width / 4) {
+        image(img[2], width / 2, height / 2, img[0].width / 2, img[0].height / 2);
+      } else {
+        if (mouseX > width / 2 + width / 4) {
+          image(img[3], width / 2, height / 2, img[0].width / 2, img[0].height / 2);
+        }
+      }
+    }
+  }
+  mySong.amp(mouseX / width);
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
